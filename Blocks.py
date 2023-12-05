715226e99c026293814fd76138c68afeb8eee7c2
meemoo23 <151596518+meemoo23@users.noreply.github.com>
2023-12-05 09:12:19 +0400

    Blocks.py

--- Blocks.py
+++ Blocks.py
@@ -1,7 +1,13 @@
+from colors import Colors
+
+
 class Block:
   def_init_(self, id):
-  self.id = id
-  self.cells = {}
-  self.cell_size = 30
-  self.rotation_state = 0
-  
+    self.id = id
+    self.cells = {}
+    self.cell_size = 30
+    self.rotation_state = 0
+    self.colors = Colors.get_cell_colors()
+
+  def draw(self, screen):
+    
