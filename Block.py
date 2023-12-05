a2c1b3c11a1d48714187c25f38ac89bcd352ae49
meemoo23 <151596518+meemoo23@users.noreply.github.com>
2023-12-05 09:33:49 +0400

    Block.py

--- Block.py
+++ Block.py
@@ -1,5 +1,5 @@
 from colors import Colors
-
+import pygame
 
 class Block:
   def_init_(self, id):
@@ -10,4 +10,7 @@ class Block:
     self.colors = Colors.get_cell_colors()
 
   def draw(self, screen):
-    
+    tiles = self.cells[self.rotation_state]
+    for tile in tiles:
+      tiles_rect = pygame.Rect(title.column * self.cell_size + 1, title.row * self.cell_size + 1, self.cell_size -1, self.cell_size -1)
+      pygame.draw.rect(screen, self.colors[self.id], title_rect)
