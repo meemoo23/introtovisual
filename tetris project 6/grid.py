a76cd19abc74055be6cb456b996eb45bea4496f3
meemoo23 <151596518+meemoo23@users.noreply.github.com>
2023-12-14 13:56:16 +0900

    Add files via upload

---
+++ tetris project 6/grid.py
@@ -0,0 +1,67 @@
+#20 rows and 10 columns - top left cell will be 0,0 representing the origin. 
+# To represent the gid a 2d array (lists of lists) will be used- empty cells are represented by 0, when a block is placed, 
+# the cells will be assigned a value depending on its color (7 in total). The moving block is not reflected in the array. 
+
+import pygame
+from colors import Colors
+
+class Grid:
+    def __init__(self):
+        self.num_rows = 20
+        self.num_cols = 10
+        self.cell_size = 30 
+        self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)] #Creates a list of 0s in 20 rows and 10 columns
+        self.colors = Colors.get_cell_colors()
+        
+    def print_grid(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                print(self.grid[row][column], end = " ")
+            print()
+
+    def is_inside(self, row, column): #Creates a boundary so the blocks cannot move outside of the grid space
+        if row >= 0 and row < self.num_rows and column >= 0 and column < self.num_cols:
+            return True
+        return False
+    
+    def is_empty(self, row, column): #This checks if a cell block is empty (Blocks cannot fall ontop of one another)
+        if self.grid[row][column] == 0:
+            return True
+        return False
+        
+    def is_row_full(self, row):#Checks if any of the cells in a row are empty (self-deleting completed lines)
+        for column in range(self.num_cols):
+            if self.grid[row][column] == 0:
+                return False
+        return True
+
+    def clear_row(self, row): #Clears the row if all cells in a row are occupied
+        for column in range(self.num_cols):
+            self.grid[row][column] = 0
+
+    def move_row_down(self, row, num_rows): #Move the blocks occupied above a full line down to the bottom
+        for column in range(self.num_cols):
+            self.grid[row+num_rows][column] = self.grid[row][column]
+            self.grid[row][column] = 0
+    
+    def clear_full_rows(self): #This function will scan all the rows in the grid to see if there are any that are full
+        completed = 0
+        for row in range(self.num_rows-1, 0, -1): #Scans all rows in reverse order (-1 to move upward)
+            if self.is_row_full(row):
+                self.clear_row(row)
+                completed += 1
+            elif completed > 0:
+                self.move_row_down(row, completed)
+        return completed
+    
+    def reset(self):
+        for row in range(self.num_rows): #Set all cells to value 0 
+            for column in range(self.num_cols):
+                self.grid[row][column] = 0
+
+    def draw(self, screen):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                cell_value = self.grid[row][column] #assigns the value of the cell to the variables and their values
+                cell_rect = pygame.Rect(column*self.cell_size +11, row*self.cell_size +11, self.cell_size -1, self.cell_size -1) #esentially; x, y (coordinates), w (width), h (height) -- Rect is the cell (invisible). The +1 is used to create a margin for the cell blocks.
+                pygame.draw.rect(screen, self.colors[cell_value], cell_rect)
---
+++ tetris project 6/main.py
@@ -0,0 +1,63 @@
+import pygame, sys
+from game import Game
+from colors import Colors
+from blocks import *
+
+pygame.init()
+
+font_path = 'modern-tetris.ttf'
+title_font = pygame.font.Font(font_path, 20) #Title font and size
+score_surface = title_font.render("Your Score:", True, Colors.white)
+next_surface = title_font.render("Next Block:", True, Colors.white)
+game_over_surface = title_font.render("GAME OVER", True, Colors.white)
+
+score_rect = pygame.Rect(350, 60, 210, 60) #Score Rectangle
+next_rect = pygame.Rect(350, 220, 210, 180)
+
+screen = pygame.display.set_mode((600,620))
+pygame.display.set_caption("My Final Project")
+
+clock = pygame.time.Clock()
+
+game = Game()
+
+GAME_UPDATE = pygame.USEREVENT #A custom event in pygame that (in this game) creates an event for every time the block's position is updated
+pygame.time.set_timer(GAME_UPDATE, 200) #The block should not have the same FPS, it needs to be slowed down to fall gradually (The block updates every 200 miliseconds)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit
+        if event.type == pygame.KEYDOWN:
+            if game.game_over == True: 
+                game.game_over = False
+                game.reset()
+            if event.key == pygame.K_LEFT and game.game_over == False: #If the player presses the left arrow key, the block must move one cell to the left
+                game.move_left()
+            if event.key == pygame.K_RIGHT and game.game_over == False: #Right key, moves to the right
+                game.move_right()
+            if event.key == pygame.K_DOWN and game.game_over == False: #Down key, moves down
+                game.move_down()
+                game.update_score(0, 1) #1 point is added everytime
+            if event.key == pygame.K_UP and game.game_over == False: #Up key, rotates the blocks 
+                game.rotate()
+        if event.type == GAME_UPDATE and game.game_over == False: #Game must stop ending if there is Game Over/ Press any key to reset the game
+            game.move_down()
+
+    score_value_surface = title_font.render(str(game.score), True, Colors.white) #Inside the game loop since it is not a static text and is dependent on the score
+
+    screen.fill(Colors.black)
+    screen.blit(score_surface, (365, 20, 50, 50)) #Block image transfer (A screen on a screen)
+    screen.blit(next_surface, (365, 180, 50, 50))
+
+    if game.game_over == True:
+        screen.blit(game_over_surface, (365, 450, 50, 50)) #Game Over is only displayed if the game is actually over
+
+    pygame.draw.rect(screen, Colors.darker_grey, score_rect, 0, 10)
+    screen.blit(score_value_surface, score_value_surface.get_rect(centerx = score_rect.centerx, centery = score_rect.centery)) #Since the score values get bigger, this function will guarantee that the score always stays in the middle
+    pygame.draw.rect(screen, Colors.darker_grey,next_rect, 0, 10)
+    game.draw(screen)
+   
+    pygame.display.update()
+    clock.tick(60) #FPS rate
